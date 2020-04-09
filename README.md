a2eee8dd584bb86851fe62600656f030591fb04c
Diogo Oliveira <diogo.oliveira44@gmail.com>
2020-04-08 22:24:25 -0300

    added options for download

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Bash scripts
 
-This repository contains some scripts used to practice bash scripts.
+This repository contains some scripts created while practicing bash scripts. 
 
-## TODO
+## Working now
 
-- [] Create a simple UI for the script download_youtube_videos.
+- [x] Create a simple UI for the script download_youtube_videos.
 - [] Create a simple UI for the script git_clone
+- [] Create a script to configure my monitor.
+- [] Create a script to to configure a dockerfile and docker-compose
+- [x] Create a script to configure git on a new directory
+- [] Create a script to read a file line-by-line.
+
--- download_youtube_videos.sh
+++ download_youtube_videos.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 # execute the script with sudo/root permissions, on the first time.
+apt-cache policy youtube-dl
+if [ $? -eq 1 ]; then
+  echo "------------------------"
+  echo 'Installing dependecies: youtube-dl'
+  sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+  sudo chmod a+rx /usr/local/bin/youtube-dl
+  echo "------------------------"
+fi
 
-echo "------------------------"
-echo 'Installing dependecies: youtube-dl'
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
-echo "------------------------"
 
-read -p 'Digite o link do youtube que você quer salvar: ' link_youtube
+download_one_video()
+{
+  read -p 'Paste a link to download: ' link_youtube
+  youtube-dl $link_youtube
+}
 
-youtube-dl $link_youtube
+download_video_via_batch_file()
+{
+  read -p 'Paste route to file: ' batch_file
+  youtube-dl -a $batch_file
+}
+
+download_playlist()
+{
+  read -p 'Paste route to file, example: ../teste: ' batch_file
+  youtube-dl -yes-playlist $batch_file
+}
+
+while [  "$option" != 0 ]
+do
+  echo '----------------------------------------------------------------------'
+  echo '                    Select number option'
+  echo '----------------------------------------------------------------------'
+  echo '[1] download one video'
+  echo '[2] download videos via batch file'
+  echo '[3] download playlist'
+  echo '[0] Exit'
+  read -p 'Select option: ' option
+  case $option in
+    1) download_one_video;;
+    2) download_video_via_batch_file;;
+    3) download_playlist;;
+  esac
+done
